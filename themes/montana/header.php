739f7561e802533e20763e47947089f8b850fb3f
Daniel Miranda <mirand@raidho.mx>
2017-05-04 14:00:44 -0500

    Getting better <head>

--- themes/montana/header.php
+++ themes/montana/header.php
@@ -1,23 +1,35 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width" />
-	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-	<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-	<div id="wrapper" class="hfeed">
-		<header id="header" role="banner">
-			<section id="branding">
-				<div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
-				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-			</section>
-			<nav id="menu" role="navigation">
-				<div id="search">
-					<?php get_search_form(); ?>
-				</div>
-				<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-			</nav>
-		</header>
-		<div id="container">
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="viewport" content="width=device-width" />
+		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+
+		<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700" rel="stylesheet">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+
+		<?php wp_head(); ?>
+	</head>
+
+
+	<body <?php body_class(); ?>>
+		<div id="wrapper" class="hfeed">
+			<header id="header" role="banner">
+				<section id="branding">
+					<div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
+					<div id="site-description"><?php bloginfo( 'description' ); ?></div>
+				</section>
+				<nav id="menu" role="navigation">
+					<div id="search">
+						<?php get_search_form(); ?>
+					</div>
+					<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+				</nav>
+			</header>
+			<div id="container">
