f51c664e418be6e86d8d21baaddeaebd2a2009ff
Daniel Miranda <mirand@raidho.mx>
2017-05-11 09:38:18 -0500

    footer markup

--- themes/montana/footer.php
+++ themes/montana/footer.php
@@ -1,8 +1,33 @@
 
 		</div>
 		<footer id="footer" role="contentinfo">
-			<div id="copyright">
-				<?php // echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
+			<div class="social">
+				<form class="" action="index.html" method="post">
+					<input type="text" name="" value="" placeholder="Suscríbete a nuestro Newsletter">
+					<input type="submit" name="" value="Enviar">
+				</form>
+				<ul>
+					<li><a href="#"><img src="http://placehold.it/48" alt=""></a></li>
+					<li><a href="#"><img src="http://placehold.it/48" alt=""></a></li>
+					<li><a href="#"><img src="http://placehold.it/48" alt=""></a></li>
+				</ul>
+			</div>
+			<div class="sitemap">
+				<ul>
+					<li><a href="#">Privacidad</a></li>
+					<li><a href="#">Transparencia</a></li>
+					<li><a href="#">Leyes</a></li>
+					<li><a href="#">Políticas Culturales</a></li>
+					<li><a href="#">Estructura Organica</a></li>
+				</ul>
+				<div id="copyright">
+					<p>CENTRO DE LAS ARTES <br>
+					Parque Fundidora Av. Fundidora y Adolfo Prieto, <br>
+					Col. Obrera, C.P. 64010, Monterrey, Nuevo León. <br>
+					T. +52 (81) 2315 1101</p>
+					<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
+					<a href="#" id="logo_nl"><img src="http://placehold.it/180x80&text=Nuevo+Leon" alt=""></a>
+				</div>
 			</div>
 		</footer>
 	</div>
