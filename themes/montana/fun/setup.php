96638cd9a1f0f3f9f4060e4fb02a51170e460a11
raidho-mx <miranda@raidho.mx>
2017-05-19 11:28:42 -0500

    IMG Sizes & stuff

--- themes/montana/fun/setup.php
+++ themes/montana/fun/setup.php
@@ -9,9 +9,12 @@
 
 		// Add Thumbnail Theme Support
 		add_theme_support('post-thumbnails');
-		// add_image_size('medium', 450, '', true); // Medium Thumbnail
-		// add_image_size('large', 1200, '', true); // Large Thumbnail
-		// add_image_size('huge', 2000, '', true); // Huge Thumbnail
+		set_post_thumbnail_size( 350, 180, array( 'center', 'center'));
+		add_image_size('medium', 700, 360, array( 'center', 'center')); // Big Cards
+		add_image_size('medium-post', 700, ''); // Medium Post content
+		add_image_size('large', 1100, 560); // Large Images
+		add_image_size('huge', 1660, ''); // Huge Thumbnail
+		add_image_size('poster', 260, 400, array( 'center', 'center')); // Movie Posters
 	}
 
 
@@ -68,39 +71,39 @@
 
 //    2 Borrar tamaño original de disco y opción
 
-	// function replace_uploaded_image($image_data) {
-	// 	// if there is no large image : return
-	// 	if (!isset($image_data['sizes']['huge'])) return $image_data;
-	//
-	// 	// paths to the uploaded image and the large image
-	// 	$upload_dir = wp_upload_dir();
-	// 	$uploaded_image_location = $upload_dir['basedir'] . '/' .$image_data['file'];
-	// 	$large_image_filename = $image_data['sizes']['huge']['file'];
-	//
-	// 	// Do what wordpress does in image_downsize() ... just replace the filenames ;)
-	// 	$image_basename = wp_basename($uploaded_image_location);
-	// 	$large_image_location = str_replace($image_basename, $large_image_filename, $uploaded_image_location);
-	//
-	// 	// delete the uploaded image
-	// 	unlink($uploaded_image_location);
-	//
-	// 	// rename the large image
-	// 	rename($large_image_location, $uploaded_image_location);
-	//
-	// 	// update image metadata and return them
-	// 	$image_data['width'] = $image_data['sizes']['huge']['width'];
-	// 	$image_data['height'] = $image_data['sizes']['huge']['height'];
-	// 	unset($image_data['sizes']['huge']);
-	//
-	// 	// Check if other size-configurations link to the large-file
-	// 	foreach($image_data['sizes'] as $size => $sizeData) {
-	// 	  if ($sizeData['file'] === $large_image_filename)
-	// 	      unset($image_data['sizes'][$size]);
-	// 	}
-	//
-	// 	return $image_data;
-	// }
-	// add_filter('wp_generate_attachment_metadata', 'replace_uploaded_image');
+	function replace_uploaded_image($image_data) {
+		// if there is no large image : return
+		if (!isset($image_data['sizes']['huge'])) return $image_data;
+
+		// paths to the uploaded image and the large image
+		$upload_dir = wp_upload_dir();
+		$uploaded_image_location = $upload_dir['basedir'] . '/' .$image_data['file'];
+		$large_image_filename = $image_data['sizes']['huge']['file'];
+
+		// Do what wordpress does in image_downsize() ... just replace the filenames ;)
+		$image_basename = wp_basename($uploaded_image_location);
+		$large_image_location = str_replace($image_basename, $large_image_filename, $uploaded_image_location);
+
+		// delete the uploaded image
+		unlink($uploaded_image_location);
+
+		// rename the large image
+		rename($large_image_location, $uploaded_image_location);
+
+		// update image metadata and return them
+		$image_data['width'] = $image_data['sizes']['huge']['width'];
+		$image_data['height'] = $image_data['sizes']['huge']['height'];
+		unset($image_data['sizes']['huge']);
+
+		// Check if other size-configurations link to the large-file
+		foreach($image_data['sizes'] as $size => $sizeData) {
+		  if ($sizeData['file'] === $large_image_filename)
+		      unset($image_data['sizes'][$size]);
+		}
+
+		return $image_data;
+	}
+	add_filter('wp_generate_attachment_metadata', 'replace_uploaded_image');
 
 
 
