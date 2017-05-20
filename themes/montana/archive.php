9e681003552a0a671d139846548e7501528179d6
Daniel Miranda <mirand@raidho.mx>
2017-05-19 19:04:02 -0500

    Filter dev mode: on

--- themes/montana/archive.php
+++ themes/montana/archive.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <section id="content" role="main">
-
+<?php /*
 		<div class="area blurry_bg">
 			<h2 class="area_title">No te pierdas</h2>
 			<div class="deck max_wrap"><?php
@@ -16,7 +16,7 @@
 				echo cards(6, 'fours'); // sixths?>
 			</div>
 		</div>
-
+*/ ?>
 
 		<div class="area" id="agenda">
 			<div class="max_wrap">
@@ -45,44 +45,96 @@
 			<div class="ag_results">
 
 				<div class="external">
-					<h3 class="max_wrap">Eventos CONARTE</h3>
+					<h3 class="max_wrap">Eventos CONARTE</h3><?php
+
+					// $the_query = new WP_Query( $args );
+
+					if ( have_posts() ) { ?>
 					<ul><?php
-				function row() { ?>
-<li>
-	<div class="max_wrap">
-		<div class="hour">
-			<p>10:00 - 23:00 <br>
-			Hasta Marzo 24</p>
-		</div>
-		<div class="">
-			<img src="http://placehold.it/170x70" alt="">
-		</div>
-		<div class="">
-			<h2>Cuentos en Kamishibai</h2>
-			<p>En los últimos tiempos han aparecido nuevas
-y muy diversas manifestaciones artísticas...</p>
-		</div>
-		<div class="">
-			<p>Centro de las Artes</p>
-		</div>
-	</div>
-</li><?php
-				}
-					row();
-					row();
-					row();
-					row();
-					row(); ?>
-					</ul>
+						while ( have_posts() ) {
+							the_post(); ?>
+							<li>
+								<a class="max_wrap" href="<?php the_permalink(); ?>">
+									<div class="schedule">
+										<p><strong><?php echo schedule_hours(); ?></strong> <br>
+										<?php echo schedule_days(); ?>
+										</p>
+									</div>
+									<div class="thumbnail">
+										<?php the_post_thumbnail(); ?>
+										<!-- <img src="http://placehold.it/170x70" alt=""> -->
+									</div>
+									<div class="title">
+										<h2><?php the_title(); ?></h2>
+										<p>...</p>
+									</div>
+									<div class="location">
+										<p><strong>Centro de las Artes</strong></p>
+									</div>
+								</a>
+							</li>
+							<?php
+						} ?>
+					</ul><?php
+						wp_reset_postdata();
+					} else { ?>
+						<ul>
+							<li>
+								<div class="max_wrap">
+									<div class="no-events">
+										<h2>No hay eventos hoy :(</h2>
+										<p>Prueba con otra fecha o disciplina.</p>
+									</div>
+								</div>
+							</li>
+						</ul><?php
+					} ?>
 				</div>
 
 				<div class="external">
-					<h3 class="max_wrap">Eventos Externos</h3>
+					<h3 class="max_wrap">Eventos Externos</h3><?php
+
+					$the_query = new WP_Query( $args );
+
+					if ( $the_query->have_posts() ) { ?>
 					<ul><?php
-						row();
-						row();
-						row();
-					?></ul>
+						while ( $the_query->have_posts() ) {
+							$the_query->the_post(); ?>
+							<li>
+								<div class="max_wrap">
+									<div class="hour">
+										<p>10:00 - 23:00 <br>
+										Hasta Marzo 24</p>
+									</div>
+									<div class="">
+										<img src="http://placehold.it/170x70" alt="">
+									</div>
+									<div class="">
+										<h2>Cuentos en Kamishibai</h2>
+										<p>En los últimos tiempos han aparecido nuevas
+							y muy diversas manifestaciones artísticas...</p>
+									</div>
+									<div class="">
+										<p>Centro de las Artes</p>
+									</div>
+								</div>
+							</li>
+							<?php
+						} ?>
+					</ul><?php
+						wp_reset_postdata();
+					} else { ?>
+						<ul>
+							<li>
+								<div class="max_wrap">
+									<div class="no-events">
+										<h2>No hay eventos hoy :(</h2>
+										<p>Prueba con otra fecha o disciplina.</p>
+									</div>
+								</div>
+							</li>
+						</ul><?php
+					} ?>
 				</div>
 			</div>
 		</div>
