812841a91f3df6f6302956dda917cc663d16ab4b
Daniel Miranda <mirand@raidho.mx>
2017-05-21 19:24:28 -0500

    Make a process ACFs to a customfield on save

--- themes/montana/fun/other.php
+++ themes/montana/fun/other.php
@@ -26,8 +26,8 @@ function schedule_days_array() {
 
 
 	if($chosenOne == 'dates') {
-		$status .= '<br>[Dates]: ';
 
+		$status .= '<br>[Dates]: ';
 		$days = array();
 		$rows = get_field('dates_picker');
 		if($rows) {
@@ -38,24 +38,45 @@ function schedule_days_array() {
 		$status .= $days;
 		$result = $days;
 
-
 	} elseif($chosenOne == 'range') {
-		$status .= '<br>[Range]';
+
+		$status .= '<br>[Range]: ';
+		$days = array();
+
+		if( have_rows('range_date_picker') ) { while ( have_rows('range_date_picker') ) { the_row();
+			$startDate = get_sub_field('start_day');
+			$endDate = get_sub_field('end_day');
+			$weekdays = get_sub_field('weekdays');
+		}}
+
+		// loop per selected weekday
+		if($weekdays) {
+			foreach($weekdays as $row) {
+				for($i = strtotime($row, strtotime($startDate)); $i <= strtotime($endDate); $i = strtotime('+1 week', $i)) {
+					$days[] = date('l dM', $i);
+				}
+			}
+		}
+
+		$status .= $days;
+		$result = $days;
+
 	} else {
 		$status .= '[ERROR]';
 	}
 
+	// return $status;
 	return $result;
-	// return $result;
 }
 
-function schedule_days() {
+function schedule_days($format) {
+	if(empty($format)) $format = 'F j Y';
 	$allDaysArray = schedule_days_array();
 
 	if($allDaysArray) {
 		foreach($allDaysArray as $row) {
 			$originalDate = $row;
-			$newDate = date("F j Y", strtotime($originalDate));
+			$newDate = date($format, strtotime($originalDate));
 			// $today = date('Ymd');
 			// if($today == $originalDate) {
 			// 	$days[] = '<strong>'.$newDate.'</strong>';}
@@ -69,3 +90,44 @@ function schedule_days() {
 	// Check with today
 	return $string;
 }
+
+
+
+
+
+/*
+ *	Guardar un campo con un arreglo de
+ */
+
+function my_acf_save_post( $post_id ) {
+
+    // get new value
+    $value = get_field('everyday');
+
+
+    // do somethingy
+	update_field('everyday', schedule_days_array());
+    // update field to value 'working'
+}
+
+add_action('acf/save_post', 'my_acf_save_post', 20);
+
+
+
+
+
+
+
+
+
+/*
+ *	Legalize Date repeater field
+ */
+	//
+	// function my_posts_where( $where ) {
+	// 	$where = str_replace("meta_key = 'days_%_date", "meta_key LIKE 'days_%_date", $where);
+	// 	// $where = str_replace("meta_key = 'dates_%_start-day", "meta_key LIKE 'dates_%_start-day", $where);
+	// 	// $where = str_replace("meta_key = 'dates_%_end-day", "meta_key LIKE 'dates_%_end-day", $where);
+	// 	return $where;
+	// }
+	// add_filter('posts_where', 'my_posts_where');
