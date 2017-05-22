b546918dff9c3a944e88cbf1c2595f9c7bb559b4
Daniel Miranda <mirand@raidho.mx>
2017-05-21 19:19:19 -0500

    Safety & guidance first!

--- themes/montana/index.php
+++ themes/montana/index.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <section id="content" role="main">
+	<h2><strong>Sorry guys, this is Index.</strong></h2>
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<div class="">
 			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
