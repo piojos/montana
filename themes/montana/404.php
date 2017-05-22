a9bae72d703ee0a78aa0d462e6cc3396fc486672
Daniel Miranda <mirand@raidho.mx>
2017-05-21 19:16:25 -0500

    Safety & guidance first!

---
+++ themes/montana/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<section id="content" role="main">
+	<div class="max_wrap">
+		<h1><strong>404</strong></h1>
+		<h2>Sorry to say this but u lost.</h2>
+	</div>
+</section>
+
+<?php get_footer(); ?>
