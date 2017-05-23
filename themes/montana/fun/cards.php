a3b16a0779af6c54d54999852e7519ee96ad7053
Daniel Miranda <mirand@raidho.mx>
2017-05-23 17:47:16 -0500

    Optionally show hours

--- themes/montana/fun/cards.php
+++ themes/montana/fun/cards.php
@@ -1,7 +1,8 @@
 <?php
 
 
-	function movie_meta($post) {
+	function movie_meta($shHour = true) {
+		// NOTA: $post use postid?
 		if(have_rows('meta')){
 			while (have_rows('meta')) {
 				the_row();
@@ -17,9 +18,9 @@
 		$string .= '<strong>'.$mYear.' '.$mDirector.'</strong><br>';
 		if($mGenre) $string .= $mGenre['label'].' <span>•</span> ';
 		if($mLength) $string .= $mLength.' <span>•</span> ';
-		$string .= $mRating['label'].'<br>';
+		$string .= $mRating['label'].'<br><br>';
 		// MISSING: Function to show hours.
-		$string .= '<br>14:00 <span>•</span> 17:00 <span>•</span> 21:00';
+		if($shHour == true) $string .= '14:00 <span>•</span> 17:00 <span>•</span> 21:00';
 		$string .= '</p>';
 		return $string;
 	}
