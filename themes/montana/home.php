0037673615006bc1b96b6121a5e6878f4359a964
Daniel Miranda <mirand@raidho.mx>
2017-05-23 15:33:12 -0500

    Added huge slider STABLE

--- themes/montana/home.php
+++ themes/montana/home.php
@@ -4,11 +4,42 @@
 	$today = current_time('Ymd'); ?>
 
 
-	<section id="content" role="main" class="home">
-	<br><br><br><?php
+	<section id="content" role="main" class="home"><?php
 
 // slider
+	$sliderPosts = get_field('featured_slider', 2);
 
+	if($sliderPosts) {
+		echo '<div class="home slider">';
+			foreach( $sliderPosts as $post) {
+				setup_postdata($post); ?>
+			<div class="slide">
+				<a href="#">
+					<div class="bg_img" style="background-image:url(<?php the_post_thumbnail_url('huge'); ?>);"></div>
+				</a>
+				<div class="max_wrap">
+					<div class="details box">
+						<a href="<?php the_permalink(); ?>">
+							<span class="parent_label"><?php echo get_post_type(); ?></span>
+							<h2><?php the_title(); ?></h2>
+							<?php if(get_field('kicker')) echo '<p class="subtitle">'.get_field('kicker').'</p>'; ?>
+							<div class="about excerpt">
+								<?php the_content(); ?>
+							</div>
+						</a>
+						<div class="status_label">
+							<p><strong>Hasta Marzo 14</strong></p>
+							<p>Location Taxonomy</p>
+
+							<a href="#" class="share"><img src="http://placehold.it/24" alt=""></a>
+							<a href="#" class="share"><img src="http://placehold.it/24" alt=""></a>
+						</div>
+					</div>
+				</div>
+			</div><?php
+			}
+		echo '</div>';
+	}
 
 
 
