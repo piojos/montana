a392525df4ea406422a7a827b491ca59178b2921
Daniel Miranda <mirand@raidho.mx>
2017-05-23 20:17:36 -0500

    WIDGETS STABLE

--- themes/montana/inc/widgets.php
+++ themes/montana/inc/widgets.php
@@ -1,39 +1,57 @@
-<?php ?>
-<div class="widget simple">
-	<h3>Mollit anim id est laborum.</h3>
-	<hr>
-	<div class="body_content">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
-	</div>
-</div>
+<?php
 
-<div class="widget program">
-	<h3>Programa</h3>
-	<hr>
-	<div class="body_content">
-		<ol>
-			<li>Cosa <span>Mar 10 / 00:00</span></li>
-			<li>Otra Cosa <span>00:00</span></li>
-			<li>Mas Cosas <span>Mar 10</span></li>
-			<li>Muchas Cosa <span>Mar 10</span></li>
-			<li>Cosinas <span>Mar 10</span></li>
-			<li>Cosa <span>Mar 10</span></li>
-		</ol>
-	</div>
-</div>
-<div class="widget program">
-	<h3>Acerca del Instructor</h3>
-	<hr>
-	<div class="body_content">
-		<img src="http://placehold.it/400x250" alt="">
-		<p><strong>Juanita Banana</strong></p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	</div>
-</div>
-<div class="widget program">
-	<h3>trailer</h3>
-	<hr>
-	<div class="body_content">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/v7MGUNV8MxU" frameborder="0" allowfullscreen></iframe>
-	</div>
-</div>
+	function runWidget($class = '', $title = '', $content = '') { ?>
+
+		<div class="widget <?php echo $class; ?>">
+			<?php if($title) echo '<h3>'.$title.'</h3>'; ?>
+			<hr>
+			<div class="body_content">
+				<?php echo $content; ?>
+				<div class="clear"></div>
+			</div>
+		</div><?php
+	}
+
+if( have_rows('widgets') ): while ( have_rows('widgets') ) : the_row();
+
+	if( get_row_layout() == 'simple_type' ):
+		runWidget('simple', get_sub_field('title'), get_sub_field('content'));
+
+
+	elseif( get_row_layout() == 'list_type' ):
+		$content = '<ol>';
+		if(have_rows('list')) { while (have_rows('list')) { the_row();
+			$rawDate = get_sub_field('date');
+			$content .= '<li>'.get_sub_field('element');
+			if(!empty($rawDate)) {
+				$niceDate = date('l d', strtotime($rawDate));
+				$content .= '<span>'.$niceDate.'</span>';
+			}
+			$content .= '</li>';
+		}}
+		$content .= '</ol>';
+
+		runWidget('list', get_sub_field('title'), $content);
+		$content = '';
+
+
+	elseif( get_row_layout() == 'profile_type' ):
+		$p_img = get_sub_field('image');
+		$p_imgArray = wp_get_attachment_image_src($p_img, 'medium');
+		$p_name = get_sub_field('name');
+		$p_bio = get_sub_field('bio');
+		if($p_img) $content .= '<img src="'.$p_imgArray[0].'" alt="" class="fleft">';
+		if($p_name) $content .= '<p><strong>'.$p_name.'</strong></p>';
+		if($p_bio) $content .= '<p>'.$p_bio.'</p>';
+
+		runWidget('list', get_sub_field('title'), $content);
+		$content = '';
+
+
+	elseif( get_row_layout() == 'embed_type' ):
+		runWidget('embed', get_sub_field('title'), get_sub_field('embed'));
+
+
+
+	endif;
+endwhile; endif; ?>
