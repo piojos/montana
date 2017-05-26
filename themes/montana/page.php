20a6c897e75157802e62fb17fa1679bd5e01fa38
Daniel Miranda <mirand@raidho.mx>
2017-05-26 18:40:06 -0500

    Basic Page STABLE

--- themes/montana/page.php
+++ themes/montana/page.php
@@ -1,7 +1,42 @@
-<?php get_header(); ?>
+<?php
 
-<section id="content" role="main">
-	<h2><strong>This is Page</strong></h2>
+	get_header();
+	if(have_posts()) : while (have_posts()) : the_post(); ?>
+
+<section id="content" role="main" class="page">
+	<div class="header">
+
+		<div class="bg_featured_banner" style="background-image:url(<?php the_post_thumbnail_url('huge'); ?>)"></div>
+
+		<div class="max_wrap post_head loading">
+			<div class="post_info main_content_column">
+				<p class="label"><?php echo get_post_type(); ?></p>
+				<h1><?php the_title(); ?></h1>
+				<?php if(get_field('kicker')) echo '<p class="subtitle">'.get_field('kicker').'</p>'; ?>
+
+				<div class="excerpt">
+					<?php the_excerpt(); ?>
+				</div>
+
+				<div class="status_label">
+					<?php get_template_part('inc/sharer'); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="post_body <?php if(!is_singular('colecciones')) echo 'max_wrap'; ?>">
+		<div class="main_content_column">
+			<div class="widget <?php echo $class; ?>">
+				<div class="body_content">
+					<?php the_content(); ?>
+				</div>
+			</div
+		</div>
+	</div>
 </section>
 
-<?php get_footer(); ?>
+<?php
+
+	endwhile; endif;
+	get_footer(); ?>
