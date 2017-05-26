e9c3bce2bb7084d4da8e69ccc78e46a97fabfcd5
Daniel Miranda <mirand@raidho.mx>
2017-05-26 17:47:52 -0500

    Format | Generate option list | Test disciplina & lugar

--- themes/montana/fun/other.php
+++ themes/montana/fun/other.php
@@ -5,23 +5,23 @@
  *	AGENDA: Get clean array from hours
  */
 
-function schedule_hours_array() {
-	$hours = array();
-	$hoursField = get_field('schedules');
-	if($hoursField) {
-		foreach($hoursField as $hour) {
-			$hours[] = $hour['hour'];
+	function schedule_hours_array() {
+		$hours = array();
+		$hoursField = get_field('schedules');
+		if($hoursField) {
+			foreach($hoursField as $hour) {
+				$hours[] = $hour['hour'];
+			}
 		}
+		return $hours;
 	}
-	return $hours;
-}
 
 
-function schedule_hours() {
-	$allHours = schedule_hours_array();
-	$string = rtrim(implode(', ', $allHours), ',');
-	return $string;
-}
+	function schedule_hours() {
+		$allHours = schedule_hours_array();
+		$string = rtrim(implode(', ', $allHours), ',');
+		return $string;
+	}
 
 
 
@@ -30,80 +30,80 @@ function schedule_hours() {
  *	Get clean array from dates & range
  */
 
-function schedule_days_array() {
-	// 1 Get chosen method
-	// NOTES: add if $field else ⬇
-	$chosenOne = get_field('dates_options');
-	$status = $chosenOne;
-	if(!empty($chosenOne)) {$status = 'field: ok';} else {$status = 'field: ERROR';}
+	function schedule_days_array() {
+		// 1 Get chosen method
+		// NOTES: add if $field else ⬇
+		$chosenOne = get_field('dates_options');
+		$status = $chosenOne;
+		if(!empty($chosenOne)) {$status = 'field: ok';} else {$status = 'field: ERROR';}
 
 
-	if($chosenOne == 'dates') {
+		if($chosenOne == 'dates') {
 
-		$status .= '<br>[Dates]: ';
-		$days = array();
-		$rows = get_field('dates_picker');
-		if($rows) {
-			foreach($rows as $row) {
-				$days[] = $row['day'];
-			}
-		}
-		$status .= $days;
-		$result = $days;
-
-	} elseif($chosenOne == 'range') {
-
-		$status .= '<br>[Range]: ';
-		$days = array();
-
-		if( have_rows('range_date_picker') ) { while ( have_rows('range_date_picker') ) { the_row();
-			$startDate = get_sub_field('start_day');
-			$endDate = get_sub_field('end_day');
-			$weekdays = get_sub_field('weekdays');
-		}}
-
-		// loop per selected weekday
-		if($weekdays) {
-			foreach($weekdays as $row) {
-				for($i = strtotime($row, strtotime($startDate)); $i <= strtotime($endDate); $i = strtotime('+1 week', $i)) {
-					$days[] = date('Ymd', $i);
+			$status .= '<br>[Dates]: ';
+			$days = array();
+			$rows = get_field('dates_picker');
+			if($rows) {
+				foreach($rows as $row) {
+					$days[] = $row['day'];
 				}
 			}
+			$status .= $days;
+			$result = $days;
+
+		} elseif($chosenOne == 'range') {
+
+			$status .= '<br>[Range]: ';
+			$days = array();
+
+			if( have_rows('range_date_picker') ) { while ( have_rows('range_date_picker') ) { the_row();
+				$startDate = get_sub_field('start_day');
+				$endDate = get_sub_field('end_day');
+				$weekdays = get_sub_field('weekdays');
+			}}
+
+			// loop per selected weekday
+			if($weekdays) {
+				foreach($weekdays as $row) {
+					for($i = strtotime($row, strtotime($startDate)); $i <= strtotime($endDate); $i = strtotime('+1 week', $i)) {
+						$days[] = date('Ymd', $i);
+					}
+				}
+			}
+
+			$status .= $days;
+			$result = $days;
+
+		} else {
+			$status .= '[ERROR]';
 		}
 
-		$status .= $days;
-		$result = $days;
-
-	} else {
-		$status .= '[ERROR]';
+		// return $status;
+		return $result;
 	}
 
-	// return $status;
-	return $result;
-}
 
+	function schedule_days($format) {
+		if(empty($format)) $format = 'F j Y';
+		$allDaysArray = schedule_days_array();
 
-function schedule_days($format) {
-	if(empty($format)) $format = 'F j Y';
-	$allDaysArray = schedule_days_array();
-
-	if($allDaysArray) {
-		foreach($allDaysArray as $row) {
-			$originalDate = $row;
-			$newDate = date_i18n($format, strtotime($originalDate));
-			// $today = date('Ymd');
-			// if($today == $originalDate) {
-			// 	$days[] = '<strong style="color:red;">'.$newDate.'</strong>';}
-			// else {
-				$days[] = $newDate;
-			// }
+		if($allDaysArray) {
+			foreach($allDaysArray as $row) {
+				$originalDate = $row;
+				$newDate = date_i18n($format, strtotime($originalDate));
+				$today = date('Ymd');
+				// if($today == $originalDate) {
+				// 	$days[] = '<strong style="color:red;">'.$newDate.'</strong>';}
+				// else {
+					$days[] = $newDate;
+				// }
+			}
 		}
-	}
-	$string = rtrim(implode(', ', $days), ',');
+		$string = rtrim(implode(', ', $days), ',');
 
-	// Check with today
-	return $string;
-}
+		// Check with today
+		return $string;
+	}
 
 
 
@@ -113,66 +113,67 @@ function schedule_days($format) {
  *	MOVIES: Make array from days & hours
  */
 
-function movieSchedule_array($format) {
-	if(empty($format)) $format = 'Ymd';
-	$status = 'online';
-	if(have_rows('dates_picker')){ while (have_rows('dates_picker')) { the_row();
-		$day = date_i18n($format, strtotime(get_sub_field('day')));
-		if(have_rows('schedules')) {
-			$hours = array();
-			while (have_rows('schedules')) { the_row();
-			$hours[] = get_sub_field('hour');
+	function movieSchedule_array($format) {
+		if(empty($format)) $format = 'Ymd';
+		$status = 'online';
+		if(have_rows('dates_picker')){ while (have_rows('dates_picker')) { the_row();
+			$day = date_i18n($format, strtotime(get_sub_field('day')));
+			if(have_rows('schedules')) {
+				$hours = array();
+				while (have_rows('schedules')) { the_row();
+				$hours[] = get_sub_field('hour');
+				}
 			}
-		}
-		$daysList[] = array($day, $hours);
-	}}
-	return $daysList;
-}
-
-
-function movieDays_array($format) {
-	if(empty($format)) $format = 'Ymd';
-	$status = 'online';
-	$days = array();
-	if(have_rows('dates_picker')){ while (have_rows('dates_picker')) { the_row();
-		$days[] = date_i18n($format, strtotime(get_sub_field('day')));
-	}}
-	return $days;
-}
-
-
-function movieDays($format) {
-	if(empty($format)) $format = 'F j Y';
-	$status = 'online';
-	$days = array();
-	$hours = array();
-	if(have_rows('dates_picker')){ while (have_rows('dates_picker')) { the_row();
-		$days[] = date_i18n($format, strtotime(get_sub_field('day')));
-		if(have_rows('schedules')) { while (have_rows('schedules')) { the_row();
-			$hours[] = get_sub_field('hour');
+			$daysList[] = array($day, $hours);
 		}}
-	}}
-	$stDays = rtrim(implode(', ', $days), ',');
-	$stHours = rtrim(implode('&nbsp; <span>•</span> &nbsp;', $hours), ' <span>•</span> ');
-	$string = '<dt class="label">Fecha</dt><dd>'.$stDays.'</dd>';
-	$string .= '<dt class="label">Horarios</dt><dd>'.$stHours.'</dd>';
-	return $string;
-}
+		return $daysList;
+	}
 
-// function movieHoursToday() {
-// 	// Get movieSchedule_array
-// 	// On day == today display hours
 
-// 	$today = date('today');
-// 	if(have_rows('dates_picker')){ while (have_rows('dates_picker')) { the_row();
-// 		$days[] = date_i18n($format, strtotime(get_sub_field('day')));
-// 		if(have_rows('schedules')) { while (have_rows('schedules')) { the_row();
-// 			$hours[] = get_sub_field('hour');
-// 		}}
-// 	}}
-// 	$string .= '<dt class="label">Horarios</dt><dd>'.$stHours.'</dd>';
-// 	return $string;
-// }
+	function movieDays_array($format) {
+		if(empty($format)) $format = 'Ymd';
+		$status = 'online';
+		$days = array();
+		if(have_rows('dates_picker')){ while (have_rows('dates_picker')) { the_row();
+			$days[] = date_i18n($format, strtotime(get_sub_field('day')));
+		}}
+		return $days;
+	}
+
+
+	function movieDays($format) {
+		if(empty($format)) $format = 'F j Y';
+		$status = 'online';
+		$days = array();
+		$hours = array();
+		if(have_rows('dates_picker')){ while (have_rows('dates_picker')) { the_row();
+			$days[] = date_i18n($format, strtotime(get_sub_field('day')));
+			if(have_rows('schedules')) { while (have_rows('schedules')) { the_row();
+				$hours[] = get_sub_field('hour');
+			}}
+		}}
+		$stDays = rtrim(implode(', ', $days), ',');
+		$stHours = rtrim(implode('&nbsp; <span>•</span> &nbsp;', $hours), ' <span>•</span> ');
+		$string = '<dt class="label">Fecha</dt><dd>'.$stDays.'</dd>';
+		$string .= '<dt class="label">Horarios</dt><dd>'.$stHours.'</dd>';
+		return $string;
+	}
+
+	// function movieHoursToday() {
+	// 	// Get movieSchedule_array
+	// 	// On day == today display hours
+
+	// 	$today = date('today');
+	// 	if(have_rows('dates_picker')){ while (have_rows('dates_picker')) { the_row();
+	// 		$days[] = date_i18n($format, strtotime(get_sub_field('day')));
+	// 		if(have_rows('schedules')) { while (have_rows('schedules')) { the_row();
+	// 			$hours[] = get_sub_field('hour');
+	// 		}}
+	// 	}}
+	// 	$string .= '<dt class="label">Horarios</dt><dd>'.$stHours.'</dd>';
+	// 	return $string;
+	// }
+
 
 
 
@@ -180,21 +181,65 @@ function movieDays($format) {
  *	Process days schedules and update $everyday
  */
 
-function everydayGen() {
-	$post_type = get_post_type($post_id);
-	if ($post_type == 'agenda') {
-		$result = schedule_days_array();
-	} elseif ($post_type == 'cineteca') {
-		$result = movieDays_array();
-	} else {}
-	return $result;
-}
+	function everydayGen() {
+		$post_type = get_post_type($post_id);
+		if ($post_type == 'agenda') {
+			$result = schedule_days_array();
+		} elseif ($post_type == 'cineteca') {
+			$result = movieDays_array();
+		} else {}
+		return $result;
+	}
 
 
 
-function my_acf_save_post( $post_id ) {
-	$value = get_field('everyday');
-	update_field('everyday', everydayGen());
-}
+	function my_acf_save_post( $post_id ) {
+		$value = get_field('everyday');
+		update_field('everyday', everydayGen());
+	}
 
-add_action('acf/save_post', 'my_acf_save_post', 20);
+	add_action('acf/save_post', 'my_acf_save_post', 20);
+
+
+
+
+
+
+
+
+/*
+*	Generate option list for Select dropdown
+*/
+
+	function listSelOptions($term) {
+		$terms = get_terms( $term );
+		if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){ foreach ( $terms as $term ) {
+			$string .= '<option value="' . $term->slug . '">' . $term->name . '</option>';
+		}}
+		return $string;
+	}
+
+
+
+
+
+
+
+
+/*
+ *	Test disciplina & lugar within post loop
+ */
+
+	function testPlaceSkill() {
+		$lugarTerm = get_term( get_field('location_picker'), 'lugares');
+		$skillArray = get_field('skill_picker');
+		$string = 'lugar: '.$lugarTerm->slug;
+		$string .= '<br>disciplina: ';
+		foreach ($skillArray as $skill) {
+			$skillTerm = get_term( $skill, 'disciplinas' );
+			$string .= $skillTerm->slug;
+		}
+		$string .= '<br>';
+		return $string;
+		// Not working. Copy&Paste
+	}
