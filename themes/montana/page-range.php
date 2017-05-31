07dba6f8a767e146572a556ec3872b33c6e9ce3c
Daniel Miranda <mirand@raidho.mx>
2017-05-31 13:51:35 -0500

    Expo, Talleres y Convos

---
+++ themes/montana/page-range.php
@@ -0,0 +1,74 @@
+<?php
+
+	/* Template Name: Slider y Rango */
+	get_header();
+
+
+	// filter
+	function my_posts_where( $where ) {
+		$where = str_replace("meta_key = 'range_date_picker_%", "meta_key LIKE 'range_date_picker_%", $where);
+		return $where;
+	}
+	add_filter('posts_where', 'my_posts_where');
+
+
+// First(Empty)
+	$post_slug = $post->post_name;
+	$today = current_time('Ymd');
+	$todayNice = date_i18n( 'l, M d Y', strtotime( $_GET['fecha'] ) );
+
+	if(htmlentities($_GET['visibleFecha']) == '') $_GET['visibleFecha'] = $todayNice;
+	if(htmlentities($_GET['fecha']) == '') $_GET['fecha'] = current_time('Ymd');
+
+	if($_GET['fecha']) { $queryDay = $_GET['fecha']; }
+	else { $queryDay = $today; }
+
+
+	$args = array(
+		'post_type' => $post_slug,
+		'meta_query' => array(
+			'relation'		=> 'AND',
+			array(
+				'key'		=> 'range_date_picker_%_start_day',
+				'compare'	=> '<=',
+				'value'		=> $queryDay,
+			),
+			array(
+				'key'		=> 'range_date_picker_%_end_day',
+				'compare'	=> '>=',
+				'value'		=> $queryDay,
+			)
+		),
+	);
+
+
+
+
+	$query = new WP_Query( $args ); ?>
+
+	<section id="content" role="main"><?php
+
+	get_template_part('inc/big', 'slider'); ?>
+
+		<div class="area" id="agenda">
+			<div class="max_wrap">
+				<h2 class="area_title">Busca <?php the_title(); ?></h2>
+				<p class="label">Estas viendo <?php echo $post_slug; ?> de:</p>
+				<form role="search" method="get" id="searchform" class="searchform ag_filter" action="<?php echo esc_url( home_url($post_slug)); ?>">
+					<div class="input_wrap <?php if($queryDay == $today) echo ' hoy'; ?>">
+						<input type="text" id="visibleFecha" value="<?php echo $_GET['visibleFecha']; ?>">
+						<input type="text" name="fecha" id="fecha" value="<?php echo $_GET['fecha']; ?>" style="display:none">
+					</div>
+					<input type="submit" value="Actualizar">
+				</form>
+			</div>
+
+			<div class="ag_results">
+				<div class="internal">
+					<?php result_list($query, get_the_title()); ?>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<?php get_footer(); ?>
