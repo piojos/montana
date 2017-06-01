4d1713406dc0a2382c7b0bcc1ae00dd228a8a507
Daniel Miranda <mirand@raidho.mx>
2017-06-01 13:41:19 -0500

    Added single metabox to talleres y exposiciones. CINE: if empty other schedules

--- themes/montana/single.php
+++ themes/montana/single.php
@@ -29,7 +29,7 @@
 					<?php get_template_part('inc/sharer'); ?>
 				</div>
 			</div><?php
-			if(is_singular(array('agenda', 'cineteca'))) {
+			if(is_singular(array('agenda', 'cineteca', 'talleres', 'exposiciones'))) {
 				get_template_part('inc/single', 'metabox');
 			} ?>
 		</div>
@@ -66,35 +66,32 @@
 		}
 
 
-
-
 		if(is_singular('cineteca')) { ?>
 			<div class="area max_wrap">
 				<h2 class="area_title">Otras fechas y horarios para <?php the_title(); ?></h2>
 				<div class="deck"><?php
-					$schedArray = movieSchedule_array('Ymd');
-					foreach ($schedArray as $key) {
-						$niceday = date_i18n('l M d', strtotime($key[0]));
-						$today = current_time('Ymd');
-						echo '<div class="schedule card fours"><div class="details box"><div class="wrap">';
-						if($today == $key[0]) echo '<strong>HOY </strong> | ';
-						if($today+1 == $key[0]) echo '<strong>MAÑANA </strong> | ';
-						echo $niceday.'<br><ul class="hours">';
-						if(is_array($key[1])){ foreach ($key[1] as $hour) {
-							echo '<li>'.$hour.'</li>';
-						}}
-						echo '</ul></div></div></div>';
-					}?>
+					$schedArray = movieFutureSchedule_array('Ymd');
+					if($schedArray) {
+						foreach ($schedArray as $key) {
+							$niceday = date_i18n('l F d', strtotime($key[0]));
+							echo '<div class="schedule card fours"><div class="details box"><div class="wrap">';
+							echo prefix_forDay($key[0]);
+							echo $niceday.'<br><ul class="hours">';
+							if(is_array($key[1])){ foreach ($key[1] as $hour) {
+								echo '<li>'.$hour.'</li>';
+							}}
+							echo '</ul></div></div></div>';
+						}
+					} else {
+						echo '<div class="schedule card fours"><div class="details box"><div class="wrap"> Esta película no se ha programado nuevamente en cartelera.</div></div></div>';
+					} ?>
 				</div>
 			</div><?php
 		} ?>
 
-
-
-
 		<div class="area max_wrap">
 			<h2 class="area_title">Mas en Cineteca</h2><?php
-
+			// Mas en $thisPostType;
 			$args = array(
 				'post_type' => 'cineteca',
 				'posts_per_page' => 4
