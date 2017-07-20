7fc2a090335e7805bf046f33db2d2f8ce8376b23
Daniel Miranda <mirand@raidho.mx>
2017-07-20 15:34:00 -0500

    Generates List of Days with posts & Deck of EXPOS & TALLERES cards.

---
+++ themes/montana/inc/single-coleccion.php
@@ -0,0 +1,130 @@
+<?php
+
+$choose = get_field('options');
+//
+// 'all_ranged'
+
+if($choose == 'auto') {
+	$selEvents = get_field('all_events');
+	$days_by_id = array();
+	$just_days = array();
+	$ids_by_day = array();
+
+
+	// 1 Makes 'everyday' array for each ID
+	foreach( $selEvents as $postID) {
+		$ed = get_field('everyday', $postID);
+		$tempA['id'] = $postID;
+		$tempA['dates'] = $ed;
+		$days_by_id[] = $tempA;
+	}
+
+
+	// 2 Makes ordered, unrepeated list of days
+	foreach($days_by_id as $ids) {
+		$just_days[] = array_values($ids['dates']);
+	}
+	$just_days_raw = array_reduce($just_days, 'array_merge', array());
+	asort($just_days_raw);
+	$just_days_nr = array_unique($just_days_raw);
+	$just_days = array_values($just_days_nr);
+
+
+	// 3 Makes list of IDs for every day
+	foreach ($just_days as $day) {
+		$thisDay = $day;
+		$temp_dates = array();
+		foreach($days_by_id as $idDates) {
+			foreach($idDates['dates'] as $datesofid){
+				if($datesofid == $thisDay) {
+					$p = 1;
+				}
+			}
+			if($p == 1) {
+				$temp_dates[] = $idDates['id'];
+			}
+			$p = 0;
+		}
+		$tempB['day'] = $thisDay;
+		$tempB['ids'] = $temp_dates;
+		$ids_by_day[] = $tempB;
+	} ?>
+
+	<ul class="ag_results"><?php
+
+	foreach ($ids_by_day as $dayM) { ?>
+		<li>
+			<div class="max_wrap">
+				<h3> <?php
+				$day = $dayM['day'];
+				$newDate = date_i18n('l F d Y', strtotime($day));
+				echo prefix_forDay($day, '<span>', '</span>').$newDate;
+				 ?></h3>
+			</div><?php
+
+			$post_objects = $dayM['ids'];
+			if( $post_objects ) { ?>
+			<ul><?php
+				foreach( $post_objects as $post) {
+					setup_postdata($post);
+					list_card('', FALSE, $day);
+				} ?>
+			</ul><?php
+			wp_reset_postdata();
+			} ?>
+		</li><?php
+	} ?>
+	</ul><?php
+
+
+
+} elseif($choose == 'manual') {
+
+	if(have_rows('event_days')) {
+		while (have_rows('event_days')) {
+			the_row(); ?>
+
+			<ul class="ag_results">
+				<li>
+					<div class="max_wrap">
+						<h3> <?php
+						$day = get_sub_field('day');
+						$newDate = date_i18n('l d \d\e F Y', strtotime($day));
+						echo prefix_forDay($day, '<span>', '</span>').$newDate;
+						 ?></h3>
+					</div><?php
+
+					$post_objects = get_sub_field('events');
+					$this_args = array(
+						'post_type' => array('agenda', 'cineteca'),
+						'post__in' => $post_objects
+					);
+
+					$thisQ = new WP_Query($this_args);
+					if($thisQ->have_posts()) { ?>
+						<ul><?php
+						while ($thisQ->have_posts()) {
+							$thisQ->the_post();
+							list_card();
+						} ?>
+					</ul><?php
+						wp_reset_postdata();
+					} ?>
+
+				</li>
+			</ul><?php
+		}
+	}
+
+} else {
+	echo 'Error.';
+}
+
+
+$ranged_args = get_field('all_ranged');
+if(!empty($ranged_args)) { ?>
+<div class="area max_wrap" style="padding: 3em 0 4em;">
+	<h2 class="area_title">Exposiciones y Talleres</h2>
+	<?php slider_deck($ranged_args, 'fours', '', TRUE); ?>
+</div><?php
+} ?>
