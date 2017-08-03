185531ddb7c5bc2b22d25bd37f239ebdee20b4b1
Daniel Miranda <mirand@raidho.mx>
2017-08-02 19:47:30 -0500

    Spanish 404

--- themes/montana/404.php
+++ themes/montana/404.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
 <section id="content" role="main">
-	<div class="max_wrap">
-		<h1><strong>404</strong></h1>
-		<h2>Sorry to say this but u lost.</h2>
+	<div class="area max_wrap">
+		<h1>404 – Esta página no existe.</h1>
+		<p>Regresa al <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" class="link">sitio principal</a>.</p>
 	</div>
 </section>
 
