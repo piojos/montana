4da07c901e1994de584c47ba4404474fe2ef7419
Daniel Miranda <mirand@raidho.mx>
2017-08-02 20:00:50 -0500

    Added hrefs

--- themes/montana/inc/sharer.php
+++ themes/montana/inc/sharer.php
@@ -1,6 +1,6 @@
-<a href="#" class="share">
+<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" class="share">
 	<svg class="icon facebook"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-facebook"></use></svg>
 </a>
-<a href="#" class="share">
+<a href="https://twitter.com/home?status=<?php the_permalink(); ?>" class="share">
 	<svg class="icon twitter"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-twitter"></use></svg>
 </a>
