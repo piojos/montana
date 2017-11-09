77bc05c87f988418d3faf12ebf4aa5a49132aa2b
raidho-mx <miranda@raidho.mx>
2017-11-09 13:30:03 -0600

    updated markup

--- themes/delcanto/search.php
+++ themes/delcanto/search.php
@@ -2,19 +2,30 @@
 
 <section id="content" role="main">
 
-	<div class="area">
+	<div class="head area">
 		<div class="max_wrap">
-			<h2 class="area_title">Buscaste: <?php echo get_search_query(); ?></h2>
-			<p><?php
+			<div class="titles">
+				<h2>Buscaste: <?php echo get_search_query(); ?></h2>
+				<p class="subtitle"><?php
 
-			$postNum = $wp_query->found_posts;
-			if($postNum == 0) {
-				echo 'No encontramos resultados.';
-			} else {
-				if($postNum >= 2) { $plural = 's'; }
-				echo 'Encontramos '.$postNum.' resultado'.$plural.'.';
-			} ?></p>
-
+				$postNum = $wp_query->found_posts;
+				if($postNum == 0) {
+					echo 'No encontramos resultados.';
+				} else {
+					if($postNum >= 2) { $plural = 's'; }
+					echo 'Encontramos '.$postNum.' resultado'.$plural.'.';
+				} ?></p>
+			</div>
+			<div class="action">
+				<form id="local_searchform" action="<?php echo home_url(); ?>" method="get">
+					<input class="inlineSearch" type="text" name="s" value="" placeholder="Buscar en CONARTE" />
+					<input class="inlineSubmit" id="searchsubmit" type="submit" alt="Search" value="Buscar" />
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="white area">
+		<div class="max_wrap">
 			<div class="deck "><?php
 
 			if ( have_posts() ) :
