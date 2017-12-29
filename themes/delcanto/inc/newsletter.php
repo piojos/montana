dc66f8c0108b6f58a7d319a47313cf2b4b1db609
raidho-mx <miranda@raidho.mx>
2017-12-29 14:25:33 -0600

    Midway through

--- themes/delcanto/inc/newsletter.php
+++ themes/delcanto/inc/newsletter.php
@@ -3,7 +3,7 @@
 		<h2>Newsletter mensual CONARTE</h2>
 		<form action="" method="post">
 			<label for="email">Tu correo:</label>
-			<input type="text" name="email" value="">
+			<input type="email" name="email" value="">
 
 			<label for="email">Selecciona temas de tu interés:</label>
 			<ul>
@@ -22,3 +22,65 @@
 		<a href="#" class="close"></a>
 	</div>
 </div>
+
+
+
+<?php /* <link href="http://fly.sendpal.mx/css/embedded.css" rel="stylesheet" type="text/css">
+<style>.subscribe-embedded-form{color: #333}.subscribe-embedded-form label{color: #555}</style>
+<div class="subscribe-embedded-form">
+<h2>Newsletter mensual de CONARTE</h2>
+<p class="text-sm text-right">
+<span class="text-danger">*</span> Indicaciones requeridas</p>
+<form action="http://fly.sendpal.mx/lists/5a3ab9edc48d2/embedded-form-subscribe-captcha" method="POST" class="form-validate-jqueryz">
+<div class="form-group control-text">
+<label>Tu correo<span class="text-danger">*</span>
+</label>
+<input id="EMAIL" placeholder="" value="" type="text" name="EMAIL" class="form-control required email ">
+</div>
+<div class="form-group control-checkboxes">
+<label>Temas de tu inter�s</label>
+<div class="checkboxes-container">
+<div class="checkbox">
+<label>
+<input type="checkbox" id="TEMAS[]" name="TEMAS[]" value="Agenda y exposiciones" class="styled"> Agenda y exposiciones </label>
+</div>
+<div class="checkbox">
+<label>
+<input type="checkbox" id="TEMAS[]" name="TEMAS[]" value="Cine" class="styled"> Cine </label>
+</div>
+<div class="checkbox">
+<label>
+<input type="checkbox" id="TEMAS[]" name="TEMAS[]" value="Convocatorias" class="styled"> Convocatorias </label>
+</div>
+<div class="checkbox">
+<label>
+<input type="checkbox" id="TEMAS[]" name="TEMAS[]" value="Talleres" class="styled"> Talleres </label>
+</div>
+<div class="checkbox">
+<label>
+<input type="checkbox" id="TEMAS[]" name="TEMAS[]" value="Ni�os" class="styled"> Ni�os </label>
+</div>
+<div class="checkbox">
+<label>
+<input type="checkbox" id="TEMAS[]" name="TEMAS[]" value="Noticias" class="styled"> Noticias </label>
+</div>
+<div class="checkbox">
+<label>
+<input type="checkbox" id="TEMAS[]" name="TEMAS[]" value="Especiales" class="styled"> Especiales </label>
+</div>
+<div class="checkbox">
+<label>
+<input type="checkbox" id="TEMAS[]" name="TEMAS[]" value="Eventos externos" class="styled"> Eventos externos </label>
+</div>
+</div>
+</div>
+<div class="form-button">
+<button class="btn btn-primary">Subscribirse</button>
+</div>
+</form>
+</div>
+<script type="text/javascript" src="http://fly.sendpal.mx/assets/js/core/libraries/jquery.min.js">
+</script>
+<script type="text/javascript" src="http://fly.sendpal.mx/assets/js/plugins/forms/validation/validate.min.js">
+</script>
+<script>$.noConflict();jQuery( document ).ready(function( $ ) {$(".subscribe-embedded-form form").validate({rules:{EMAIL: {required: true,email: true,remote: "http://fly.sendpal.mx/lists/5a3ab9edc48d2/check-email" }}});});</script> */ ?>
